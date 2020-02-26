fcf4990ef020e67a03e2ce154d954e71cde35e55
gakanjira <gakanjira@gmail.com>
2020-02-26 16:31:19 +0100

    updated project with hooks

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 
@@ -7,31 +7,31 @@ import CollectionPageContainer from "../collection/collection.container";
 
 import { startFetchingCollections } from "../../redux/shop/shop.actions";
 
-class ShopPage extends React.Component {
-  unsubscribeFromSnapshot = null;
-
-  componentDidMount() {
-    const { startFetchingCollections } = this.props;
+const ShopPage = ({ match, startFetchingCollections }) => {
+  useEffect(() => {
     startFetchingCollections();
-  }
+  }, [startFetchingCollections]);
+  // unsubscribeFromSnapshot = null;
 
-  render() {
-    const { match } = this.props;
-    return (
-      <div className="shop-page">
-        <Route
-          exact
-          path={`${match.path}`}
-          component={CollectionsOverviewContainer}
-        />
-        <Route
-          path={`${match.path}/:collectionId`}
-          component={CollectionPageContainer}
-        />
-      </div>
-    );
-  }
-}
+  // componentDidMount() {
+  //   const { startFetchingCollections } = this.props;
+  //   startFetchingCollections();
+  // }
+
+  return (
+    <div className="shop-page">
+      <Route
+        exact
+        path={`${match.path}`}
+        component={CollectionsOverviewContainer}
+      />
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionPageContainer}
+      />
+    </div>
+  );
+};
 
 const mapDispatchToProps = dispatch => ({
   startFetchingCollections: () => dispatch(startFetchingCollections())
